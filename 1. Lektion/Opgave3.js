17402572420b8913178cdd9a4beed856712fcebb
Morten Jørgensen <mortenj2@hotmail.dk>
2020-02-05 22:50:16 +0100

    Lavet opgave 6

--- 1. Lektion/Opgave3.js
+++ 1. Lektion/Opgave3.js
@@ -2,12 +2,12 @@ let array = ["Hej", "med", "dig", "jeg", "hedder", "Kaj", "bonderov"];
 
 function average_length_array(array){
     let total = 0,
-        word_count = array.length,
-        new_array = [];
+        word_count = array.length;
+        // new_array = [];
 
-    new_array = array.map(a => a.length);
-    total = new_array.reduce((total, b) => total + b);
-    //   let wordLength = strings.reduce((value, string) => value + string.length, 0);
+    // let new_array = array.map(a => a.length);
+
+    total = array.reduce((value, string) => value + string.length, 0);
 
     return total / word_count;
 }
--- 1. Lektion/Opgave6.js
+++ 1. Lektion/Opgave6.js
@@ -1,15 +1,55 @@
 let array = ["Hej", "med", "dig", "jeg", "hedder", "Kaj", "bonderov"];
-
+/*
 function average_length_array(array){
     let total = 0,
         word_count = array.length,
-        new_array = [];
+        // new_array = [];
 
-    new_array = array.map(a => a.length);
-    total = new_array.reduce((total, b) => total + b);
-    //   let wordLength = strings.reduce((value, string) => value + string.length, 0);
+    // new_array = array.map(a => a.length);
+    total = array.reduce((value, string) => value + string.length, 0);
 
     return total / word_count;
 }
 
 console.log(average_length_array(array));
+*/
+
+function banger_info(array){
+    function average_length(array){
+        let total1 = array.reduce((value, string) => value + string.length, 0);
+
+        return total1 / array.length;
+    }
+    function minimum(array){
+        let temp_array = array.map(string => string.length);
+
+        return Math.min(...temp_array);
+    }
+    function maximum(array){
+        let temp_array = array.map(string => string.length);
+
+        return Math.max(...temp_array);
+    }
+    function median(array){
+        let temp_array = array.sort((a, b) => a.length - b.length);
+
+        let half = Math.floor(array.length / 2);
+
+        if (half % 2 === 1){
+            return temp_array[half];
+        }
+
+        return (temp_array[half - 1] + temp_array[half]) / 2.0;
+    }
+
+    let object = {
+        min: minimum(array),
+        max: maximum(array),
+        average: average_length(array),
+        medi: median(array)
+    };
+
+    return object;
+}
+
+console.log(banger_info(array));
